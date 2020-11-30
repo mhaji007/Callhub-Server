099363815f51cd08cd4ebf9d639c48333a011992
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-30 00:02:03 -0500

    Create a Callhub class

--- callhub.js
+++ callhub.js
@@ -1,6 +1,26 @@
-const phoneNumber = process.env.PHONE_NUMBER;
-const phoneNumberSid = process.env.PHONE_NUMBER_SID;
-const tokenSid = process.env.TOKEN_SID;
-const tokenSecret = process.env.TOKEN_SECRET;
-const accountSid = process.env.ACCOUNT_SID;
-const serviceId= process.env.SERVICE_SID;
+const twilio = require("twilio");
+
+require("dotenv").config();
+
+class Callhub {
+  phoneNumber = process.env.PHONE_NUMBER;
+  phoneNumberSid = process.env.PHONE_NUMBER_SID;
+  tokenSid = process.env.TOKEN_SID;
+  tokenSecret = process.env.TOKEN_SECRET;
+  accountSid = process.env.ACCOUNT_SID;
+  serviceId = process.env.SERVICE_SID;
+
+  constructor() {
+    this.client = twilio(this.tokenSid, this.tokenSecret, {
+      accountSid: this.accountSid,
+    });
+  }
+  getTwilio() {
+    this.client
+  }
+}
+
+const instance = new Callhub();
+Object.freeze(instance);
+
+module.exports = instance;
--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require("express");
+const callhub = require("./Callhub");
+
 const app = express();
+const client = callhub.client;
+
 require("dotenv").config();
 
-app.get('/test', (req, res) => {
-  res.send("Welcome to Callhub")
+app.get("/test", (req, res) => {
+  res.send("Welcome to Callhub");
 });
-app.get('/login', (req, res) => {
-  console.log('Logging in... ')
+app.get("/login", (req, res) => {
+  console.log("Logging in... ");
 });
-app.get('/verify', (req, res) => {
-  console.log('Verifying code...')
-  console.log('number', process.env.PHONE_NUMBER);
+app.get("/verify", (req, res) => {
+  console.log("Verifying code...");
 });
 
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
   console.log(`Listening on PORT ${port}`);
-})
+});
