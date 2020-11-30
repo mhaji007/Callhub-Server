edd85068684a226fcf5e4536471fb0789b1e257f
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-29 22:28:55 -0500

    Set up project structure. Set up API keys

---
+++ callhub.js
@@ -0,0 +1,5 @@
+const phoneNumber = process.env.PHONE_NUMBER;
+const phoneNumberSid = process.env.PHONE_NUMBER_SID;
+const tokenSid = process.env.TOKEN_SID;
+const tokenSecret = process.env.TOKEN_SECRET;
+const accountSid = process.env.ACCOUNT_SID;
