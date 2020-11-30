93aa3e3c0412417ecaefe50a263fc4933c34698e
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-29 23:28:27 -0500

    Set up endpoints for receiving mobile number and verification code

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
-const PORT = 3002;
+require("dotenv").config();
 
 app.get('/test', (req, res) => {
   res.send("Welcome to Callhub")
 });
+app.get('/login', (req, res) => {
+  console.log('Logging in... ')
+});
+app.get('/verify', (req, res) => {
+  console.log('Verifying code...')
+  console.log('number', process.env.PHONE_NUMBER);
+});
 
-app.listen(PORT, () => {
-  console.log(`Listening on PORT ${PORT}`);
+const port = process.env.PORT || 3002;
+
+app.listen(port, () => {
+  console.log(`Listening on PORT ${port}`);
 })
