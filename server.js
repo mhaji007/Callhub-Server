9bd09dc013d57ead23f3b1b3dbf8fdc1d2ec32b9
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-30 18:43:05 -0500

    Send code back to callhub server for verification

--- server.js
+++ server.js
@@ -8,12 +8,18 @@ require("dotenv").config();
 
 
 const app = express();
+
+// Middlewares
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/test", (req, res) => {
-  res.send("Welcome to Callhub");
-});
+// // Test endpoint
+// app.get("/test", (req, res) => {
+//   res.send("Welcome to Callhub");
+// });
+
+// Endpoint for sending verification code
+// (retruns an object containing verification code)
 app.post("/login", async (req, res) => {
   console.log("Logging in... ");
   const {to, username, channel} = req.body
@@ -21,13 +27,18 @@ app.post("/login", async (req, res) => {
   const data = await callhub.sendVerify(to, channel);
   res.send(data);
 });
-app.get("/verify", async (req, res) => {
+// Endpoint for verifying the verification code sent
+// back from the frontend
+// (returns an object containing verification code's valid status)
+app.post("/verify", async (req, res) => {
+  const {to, code} = req.body
   console.log("Verifying code...");
-  const data = await callhub.verifyCode(process.env.MOBILE, req.query.code);
-  return data;
+  const data = await callhub.verifyCode(to, code);
+  res.send(data);
 });
 
 const port = process.env.PORT || 3002;
+
 
 app.listen(port, () => {
   console.log(`Listening on PORT ${port}`);
