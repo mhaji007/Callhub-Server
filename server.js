5d94f1315488b0c4b1cd03a944bf3ea9e1cba75c
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-30 19:29:32 -0500

    Set up socket io

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const callhub = require("./Callhub");
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const http = require("http");
+// Use socket io for emitting event
+// from client to server without
+// having to post and wait on them
+const socketIo = require("socket.io");
 
 require("dotenv").config();
 
-
-
 const app = express();
+// Server
+const server = http.createServer(app);
+// Scoket
+const socket = socketIo(server);
+
+// Subscribe to events on socket
+socket.on("connection", (socket) => {
+  console.log("Socket connected", socket);
+});
 
 // Middlewares
 app.use(bodyParser.json());
@@ -22,7 +34,7 @@ app.use(cors());
 // (retruns an object containing verification code)
 app.post("/login", async (req, res) => {
   console.log("Logging in... ");
-  const {to, username, channel} = req.body
+  const { to, username, channel } = req.body;
   // Send verification code to MOBILE number
   const data = await callhub.sendVerify(to, channel);
   res.send(data);
@@ -31,7 +43,7 @@ app.post("/login", async (req, res) => {
 // back from the frontend
 // (returns an object containing verification code's valid status)
 app.post("/verify", async (req, res) => {
-  const {to, code} = req.body
+  const { to, code } = req.body;
   console.log("Verifying code...");
   const data = await callhub.verifyCode(to, code);
   res.send(data);
@@ -39,7 +51,8 @@ app.post("/verify", async (req, res) => {
 
 const port = process.env.PORT || 3002;
 
-
-app.listen(port, () => {
+// When using sockets server now takes over
+// the listening from app ( app.listen(port, () => {...})
+server.listen(port, () => {
   console.log(`Listening on PORT ${port}`);
 });
